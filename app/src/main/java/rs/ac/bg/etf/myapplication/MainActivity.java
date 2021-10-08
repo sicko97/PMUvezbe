a7c8f6dabd2e45a43dded27e1b4613d4b073b7e2
luka <lukajec97@gmail.com>
2021-10-08 23:19:34 +0200

    Initial commmit

---
+++ app/src/main/java/rs/ac/bg/etf/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package rs.ac.bg.etf.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
