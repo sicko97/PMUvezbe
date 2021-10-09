ce2cbb918c2bf972d783653df0def2e392ba62a3
luka <lukajec97@gmail.com>
2021-10-09 13:07:45 +0200

    Add ViewBinding

--- app/src/main/java/rs/ac/bg/etf/myapplication/MainActivity.java
+++ app/src/main/java/rs/ac/bg/etf/myapplication/MainActivity.java
@@ -3,12 +3,26 @@ package rs.ac.bg.etf.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import rs.ac.bg.etf.myapplication.databinding.ActivityMainBinding;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
     }
 }
