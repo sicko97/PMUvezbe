16d7b0014dc53d925d4d8f9d2371204f92c390dd
luka <lukajec97@gmail.com>
2021-12-22 22:31:01 +0100

    Add onClickListener for create button

--- app/src/main/java/rs/ac/bg/etf/myapplication/workout/WorkoutCreateFragment.java
+++ app/src/main/java/rs/ac/bg/etf/myapplication/workout/WorkoutCreateFragment.java
@@ -15,10 +15,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.Format;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Date;
 
 import rs.ac.bg.etf.myapplication.MainActivity;
 import rs.ac.bg.etf.myapplication.R;
+import rs.ac.bg.etf.myapplication.data.RunDatabase;
+import rs.ac.bg.etf.myapplication.data.Workout;
 import rs.ac.bg.etf.myapplication.databinding.FragmentWorkoutCreateBinding;
 
 public class WorkoutCreateFragment extends Fragment {
@@ -43,21 +50,43 @@ public class WorkoutCreateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        binding = FragmentWorkoutCreateBinding.inflate(inflater,container,false);
+        binding = FragmentWorkoutCreateBinding.inflate(inflater, container, false);
 
 
         binding.toolbar.setNavigationOnClickListener(
-                view-> navController.navigateUp());
+                view -> navController.navigateUp());
 
         binding.date1.setOnClickListener(
-                view -> new DatePickerFragment().show(getChildFragmentManager(),null));
+                view -> new DatePickerFragment().show(getChildFragmentManager(), null));
 
-        getChildFragmentManager().setFragmentResultListener(REQUEST_KEY,this,
+        getChildFragmentManager().setFragmentResultListener(REQUEST_KEY, this,
                 ((requestKey, result) -> {
                     Date date = (Date) result.getSerializable(DatePickerFragment.SET_DATE_KEY);
                     String dateForEditText = DateTimeUtil.getSimpleDateFormat().format(date);
                     binding.workoutDate.getEditText().setText(dateForEditText);
                 }));
+
+        binding.workoutCreate.setOnClickListener(view -> {
+
+            Date date = (Date) parse(binding.workoutDate, DateTimeUtil.getSimpleDateFormat());
+            String label = (String) parse(binding.workoutLabel, null);
+            Number distance = (Number) parse(binding.workoutDistance, NumberFormat.getInstance());
+            Number duration = (Number) parse(binding.workoutDuration, NumberFormat.getInstance());
+
+            if (!(date == null || distance == null || label == null || duration == null)) {
+                RunDatabase runDatabase = RunDatabase.getInstance(mainActivity);
+                runDatabase.workoutDao().insert(new Workout(
+                        0,
+                        date,
+                        label,
+                        distance.doubleValue(),
+                        duration.doubleValue()
+                ));
+
+                navController.navigateUp();
+            }
+
+        });
 
 
         return binding.getRoot();
@@ -68,4 +97,28 @@ public class WorkoutCreateFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
     }
+
+    private Object parse(TextInputLayout textInputLayout, Format format) {
+        Object result;
+        try {
+            String inputString = textInputLayout.getEditText().getText().toString();
+            if (!inputString.equals("")) {
+                if (format != null) {
+                    result = format.parseObject(inputString);
+                } else {
+                    result = inputString;
+                }
+                textInputLayout.setError(null);
+            } else {
+                textInputLayout.setError(
+                        mainActivity.getResources().getString(R.string.workout_create_error_empty));
+                result = null;
+            }
+        } catch (ParseException e) {
+            textInputLayout.setError("Wrong format");
+            return null;
+        }
+        return result;
+    }
+
 }
